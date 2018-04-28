ae7dadb8b29f4dea636462a33318ecaab22be631
Promethivm <4203892+Promethivm@users.noreply.github.com>
2018-04-28 19:49:39 -0400

    Create ok.cpp

---
+++ ok.cpp
@@ -0,0 +1,11 @@
+     // Escape key pressed
+            if ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Escape))
+                App.Close();
+            
+            // right key pressed
+            if ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Right))
+                right_pressed=1;
+            
+            // right key pressed
+            if ((Event.Type == sf::Event::KeyReleased) && (Event.Key.Code == sf::Key::Right))
+                right_pressed=0;
